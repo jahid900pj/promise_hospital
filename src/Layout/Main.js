b8e2f1b3409e602212b0ba40d87646e3fb982afa
tantaana <shariartanvirofficial@gmail.com>
2023-01-13 02:45:48 +0600

    bad bunny

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-<<<<<<< HEAD
 import Advertise from '../Pages/Home/Advertise/Advertise';
 import Nav1 from '../Pages/Shared/NavBar/Nav1/Nav1';
 import Nav2 from '../Pages/Shared/NavBar/Nav2/Nav2';
-=======
 import Footer from '../Pages/Shared/Footer/Footer';
->>>>>>> rakib
 
 const Main = () => {
     return (
