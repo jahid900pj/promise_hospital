a161b99297736b36f29e04ea06fbaef5598db412
tantaana <shariartanvirofficial@gmail.com>
2023-01-13 02:26:45 +0600

    another from tanvir

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Advertise from '../Pages/Home/Advertise/Advertise';
+import Nav1 from '../Pages/Shared/NavBar/Nav1/Nav1';
+import Nav2 from '../Pages/Shared/NavBar/Nav2/Nav2';
 
 const Main = () => {
     return (
         <div>
-
+            <Advertise></Advertise>
+            <Nav1></Nav1>
+            <Nav2></Nav2>
             <Outlet></Outlet>
 
         </div>
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Advertise from '../Advertise/Advertise';
+import HomeCarousel from '../HomeCarousel/HomeCarousel';
 
 const Home = () => {
     return (
         <div>
-
+            <HomeCarousel></HomeCarousel>
         </div>
     );
 };
