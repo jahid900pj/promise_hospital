ea695a602580dabb05a452939c76b8f847b0a245
Rokibul-bd <mdrokbiulislamcmt176@gmail.com>
2023-01-13 01:33:21 +0600

    footer components Created

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p>Footer Section has comming son.</p>
+        </div>
+    );
+};
+
+export default Footer;
