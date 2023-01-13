84a5b433d8b0867810226c1a3efba0ee78c42f51
jahidhasan234031 <jahidhasan234031@mail.com>
2023-01-13 17:43:31 +0600

    work done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ChooseManipal from '../InternationalCenters/ChooseManipal';
+import MedicalValue from '../MedicalValue/MedicalValue';
 
 const Home = () => {
     return (
         <div>
             <ChooseManipal></ChooseManipal>
+            <MedicalValue></MedicalValue>
         </div>
     );
 };
